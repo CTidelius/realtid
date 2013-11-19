867ae2c6f77d35793c03661c381961315fb93dc9
digli <elliot.j@live.se>
2013-11-19 11:23:13 +0100

    Changed ImageStruct to create image and timestamp from a byte array

--- src/realtime/client/ImageStruct.java
+++ src/realtime/client/ImageStruct.java
@@ -1,19 +1,44 @@
 package realtime.client;
 
+import java.nio.ByteBuffer;
+
+import se.lth.cs.cameraproxy.Axis211A;
+
 public class ImageStruct {
 
 	private byte[] image;
 	private long timestamp;
-	
-	public ImageStruct(byte[] image, long timestamp) {
-		this.image = image;
-		this.timestamp = timestamp;
+
+	public ImageStruct(byte[] data) {
+		timestamp = createTimestamp(data);
+		image = createImage(data);
 	}
-	
+
+	// Create timestamp out of data array
+	private long createTimestamp(byte[] data) {
+		byte[] time = new byte[8];
+		for (int i = 0; i < 8; i++) {
+			time[i] = data[i + 1];
+		}
+		ByteBuffer buffer = ByteBuffer.allocate(8);
+		buffer.put(time);
+		buffer.flip(); // need flip
+		return buffer.getLong();
+	}
+
+	// Create image array out of data array
+	private byte[] createImage(byte[] data) {
+		byte[] tempImage = new byte[Axis211A.IMAGE_BUFFER_SIZE];
+		for (int i = 0; i < image.length; i++) {
+			image[i] = data[i + 9];
+		}
+		return tempImage;
+	}
+
 	public byte[] getImage() {
 		return image;
 	}
-	
+
 	public long timestamp() {
 		return timestamp;
 	}
