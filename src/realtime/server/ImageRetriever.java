18e7f310acd1d230640fd6720ae58da296bf9773
Evozer <dat11msi@student.lu.se>
2013-11-19 10:39:44 +0100

    http client

--- src/realtime/server/ImageRetriever.java
+++ src/realtime/server/ImageRetriever.java
@@ -5,8 +5,19 @@ import se.lth.cs.fakecamera.Axis211A;
 public class ImageRetriever extends Thread {
 	private Axis211A camera;
 	private Monitor monitor;
+	private byte[] buffer;
 	
 	public ImageRetriever(Monitor monitor){
 		this.monitor = monitor;
+		this.camera = new Axis211A();
+		buffer = new byte[Axis211A.IMAGE_BUFFER_SIZE];
+	}
+	
+	public void run() {
+		camera.connect();
+		while(!isInterrupted()) {
+			camera.getJPEG(buffer, 0);
+			monitor.putImage(buffer);
+		}
 	}
 }
--- src/realtime/server/Monitor.java
+++ src/realtime/server/Monitor.java
@@ -1,5 +1,22 @@
 package realtime.server;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Monitor {
 	private boolean isIdle;
+	private byte[] lastImage;
+	private Queue<Integer> messagesToSend;
+	
+	private ImageRetriever imageRetriever;
+	private ReceiverThread reciever;
+	private SenderThread sender;
+	
+	public Monitor() {
+		messagesToSend = new ArrayDeque<Integer>();
+	}
+	
+	public synchronized void putImage(byte[] bytes) {
+		lastImage = bytes;
+	}
 }
