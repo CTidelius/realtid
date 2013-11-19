212d6bf6395ee052a99212294c7d6cd7327c7c13
digli <elliot.j@live.se>
2013-11-19 10:39:55 +0100

    fixade konstruktor i DisplayHandler

--- src/realtime/client/DisplayHandler.java
+++ src/realtime/client/DisplayHandler.java
@@ -3,6 +3,12 @@ package realtime.client;
 public class DisplayHandler extends Thread {
 	private Buffer buffer;
 	private GUI gui;
-	public DisplayHandler() {
+	public DisplayHandler(GUI gui, Buffer buffer) {
+		this.gui = gui;
+		this.buffer = buffer;
+	}
+	
+	public void run() {
+		
 	}
 }
