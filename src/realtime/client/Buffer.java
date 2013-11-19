b5d81b2b218a82534b5abf59c104c65050c4b0a9
digli <elliot.j@live.se>
2013-11-19 11:58:49 +0100

    added camIndex to ImageStruct and changed to PriorityQueues in the Buffer

--- src/realtime/client/Buffer.java
+++ src/realtime/client/Buffer.java
@@ -1,6 +1,7 @@
 package realtime.client;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class Buffer {
 
@@ -8,14 +9,21 @@ public class Buffer {
 	public static final int MODE_SYNCH = 1;
 
 	private int mode;
-	private ArrayList<ArrayList<ImageStruct>> images;
+	private ArrayList<PriorityQueue<ImageStruct>> images;
 
-	public Buffer() {
+	public Buffer(int nbrOfCameras) {
 		mode = MODE_ASYNCH;
-		images = new ArrayList<ArrayList<ImageStruct>>();
+		images = new ArrayList<PriorityQueue<ImageStruct>>();
+		for (int i = 0; i < nbrOfCameras; i++) {
+			images.add(new PriorityQueue<ImageStruct>());
+		}
 	}
 
 	public synchronized int getMode() {
 		return mode;
+	}
+	
+	public synchronized void putImage(ImageStruct image) {
+		images.get(image.getIndex()).add(image);
 	}
 }
